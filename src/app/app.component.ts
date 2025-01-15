177af4a85f756f813a3b638a3596a65d8fb4de52
kul-arun <kul.arun21@gmail.com>
2025-01-15 05:08:21 +0100

    Remove default code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
